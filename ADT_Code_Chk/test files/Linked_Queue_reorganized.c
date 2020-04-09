a0c5d4da35fd77478075b01692c96ce2bfa030b5
Olcmyk <60802501+Olcmyk@users.noreply.github.com>
2020-04-09 15:09:34 +0800

    就算没有这个if，两个也是相等的

--- ADT_Code_Chk/test files/Linked_Queue_reorganized.c
+++ ADT_Code_Chk/test files/Linked_Queue_reorganized.c
@@ -124,8 +124,6 @@ Status DeQueue(struct LinkQueue *Q, ElemType *E)
     struct QNode* p = Q->front->next;
     *E = p->data;
     Q->front->next = p->next;
-    if (Q->rear == p)
-        Q->rear = Q->front;
     free(p);
     return OK;
 }
